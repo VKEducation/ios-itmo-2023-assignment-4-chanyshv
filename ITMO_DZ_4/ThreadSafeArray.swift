7fab951dcd2f8bb1435c0f3c7d9f11f4c08af6e3
chanyshev <chanyshv@gmail.com>
2023-12-03 21:40:32 +0300

    done

--- ITMO_DZ_4/ThreadSafeArray.swift
+++ ITMO_DZ_4/ThreadSafeArray.swift
@@ -1,21 +1,49 @@
 import Foundation
 
-class ThreadSafeArray<T>  {
+class ThreadSafeArray<T> {
     private var array: [T] = []
+    private let lock = NSLock()
+
+    func append(_ item: T) {
+        lock.lock()
+        array.append(item)
+        lock.unlock()
+    }
+
+    func remove(at index: Int) {
+        lock.lock()
+        array.remove(at: index)
+        lock.unlock()
+    }
 }
 
 extension ThreadSafeArray: RandomAccessCollection {
     typealias Index = Int
     typealias Element = T
 
-    var startIndex: Index { return array.startIndex }
-    var endIndex: Index { return array.endIndex }
+    var startIndex: Index {
+        lock.lock()
+        defer { lock.unlock() }
+        return array.startIndex
+    }
+
+    var endIndex: Index {
+        lock.lock()
+        defer { lock.unlock() }
+        return array.endIndex
+    }
 
     subscript(index: Index) -> Element {
-        get { return array[index] }
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return array[index]
+        }
     }
 
     func index(after i: Index) -> Index {
+        lock.lock()
+        defer { lock.unlock() }
         return array.index(after: i)
     }
 }
